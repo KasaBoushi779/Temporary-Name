e46f36d5d7471b964c315024b53cfbee4ae97efe
KasaBoushi779 <kasanokasane779@gmail.com>
2025-10-27 01:34:46 -0400

    Finished both the MusicDatabaseKernel and MusicDatabase interfaces

--- src/MusicDatabaseKernel.java
+++ src/MusicDatabaseKernel.java
@@ -6,7 +6,13 @@ import components.standard.Standard;
  * Component meant to model a database of music with primary methods. (Note: by
  * package-wide convention, all references are non-null.)
  */
-public interface MusicDatabaseKernel extends Standard<MusicDatabase> {
+public interface MusicDatabaseKernel
+        extends Iterable<MusicDatabase>, Standard<MusicDatabase> {
+
+    /*
+     * I didn't see any iterator methods in the osu components that had them, so
+     * I assume they're not needed here?
+     */
 
     /**
      * An enum for methods only intended to accept the four fields of the
@@ -21,49 +27,69 @@ public interface MusicDatabaseKernel extends Standard<MusicDatabase> {
     }
 
     /**
-     * Adds a {@code Song} object made from the given parameters into the
-     * database.
-     *
-     * @param title
-     *            The title of the song
-     * @param artist
-     *            The artist of the song
-     * @param length
-     *            The length of the song
-     * @param album
-     *            The album the song is from
-     * @requires title, artist, and length are neither null nor ""
-     * @ensures {@code this = #this * song}
-     * @updates {@code this}
+     * An internal class containing song data, with simple methods to get
+     * parameters (title, artist, album, length).
      */
-    void addEntry(String title, String artist, String length, String album);
+    interface Song {
+
+        /**
+         * Returns {@code this.title}.
+         *
+         * @return The title of the song
+         * @ensures title = this.title
+         */
+        String title();
+
+        /**
+         * Returns {@code this.artist}.
+         *
+         * @return The artist of the song.
+         * @ensures artist = this.artist
+         */
+        String artist();
+
+        /**
+         * Returns {@code this.length}.
+         *
+         * @return The length of the song
+         * @ensures length = this.length
+         */
+        String length();
+
+        /**
+         * Returns {@code this.album}.
+         *
+         * @return The album of the song
+         * @ensures album = this.album
+         */
+        String album();
+    }
 
     /**
-     * Adds a {@code Song} object made from the given parameters into the
-     * database. Album is left at the default value, which is an empty string.
-     *
-     * @param title
-     *            The title of the song
-     * @param artist
-     *            The artist of the song
-     * @param length
-     *            The length of the song
-     * @requires title, artist, and length are neither null nor ""
-     * @ensures {@code this = #this * song}
-     * @updates {@code this}
-     */
-    void addEntry(String title, String artist, String length);
-
-    /**
-     * Adds a premade {@code Song} object to the database.
+     * Adds a {@code Song} object to the database if a duplicate does not
+     * already exist.
      *
      * @param song
      *            An object of type {@code Song} to add to the database.
-     * @requires song != null
-     * @ensures {@code this = #this * song}
-     * @updates {@code this}
+     * @requires song != null AND an identical Song object is not already in the
+     *           database
+     * @ensures this = #this * song
+     * @updates this
      */
     void addEntry(Song song);
+
+    /**
+     * Returns the {@code Song} at position {@code n} in the database. Returns
+     * -1 if no object exists at that position.
+     *
+     * @param n
+     *            An int representing the index at which the user wants to fetch
+     *            a value from.
+     * @return A {@code Song} if one exists at {@code n}.
+     * @ensures getEntryInOrder = The {@code Song} at position {@code n} if it
+     *          exists, or -1 otherwise
+     */
+    Song getEntryInOrder(int n);
 
     /**
      * A method that takes an enum (which can be "title", "artist", "album", or
@@ -82,4 +108,57 @@ public interface MusicDatabaseKernel extends Standard<MusicDatabase> {
      *          matching the {@code value} parameter
      */
     ArrayList<Song> getEntries(SearchField field, String value);
+
+    /**
+     * A method that takes an enum (which can be "title", "artist", "album", or
+     * "length"), and a value to use to find matches within the {@code Song}
+     * objects of the database. For each match it finds, it will print out its
+     * details and ask the user if they want to remove it from the database.
+     * Every match the user chooses to remove will be returned in an
+     * {@code ArrayList}.
+     *
+     * @param field
+     *            An enum representing the {@code Song} field to search through
+     * @param value
+     *            A string value to search by
+     * @return An {@code ArrayList} of all {@code Songs} with a field matching
+     *         the given value.
+     * @requires value != null
+     * @ensures getEntries = All Songs that have a value in {@code field}
+     *          matching the {@code value} parameter
+     * @updates this
+     */
+    ArrayList<Song> removeEntries(SearchField field, String value);
+
+    /**
+     * Takes in a {@code Song} and removes it from the database.
+     *
+     * @param song
+     *            A {@code Song} object to be searched for and removed, then
+     *            returned.
+     * @return An {@code ArrayList} of all {@code Songs} removed.
+     * @requires {@code song} is in this
+     * @ensures this = #this / song
+     * @updates this
+     */
+    Song removeEntry(Song song);
+
+    /**
+     * A method that takes in a Song object and checks for a match in the
+     * database.
+     *
+     * @param song
+     *            A {@code Song} object to be searched for
+     * @return A boolean
+     * @ensures contains == true if {@code song} is in this
+     */
+    Boolean contains(Song song);
+
+    /**
+     * Returns the number of songs in the database.
+     *
+     * @return the number of songs in the database.
+     * @ensures size = this.length
+     */
+    int size();
 }
